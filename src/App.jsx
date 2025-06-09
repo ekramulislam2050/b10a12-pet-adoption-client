23ced2727e27e809b88db7dbdf38a64d284b70ff
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-06-09 11:20:36 +0600

    conmplete initial setup of client site

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+ 
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+    <div className='text-center'>
+           <h1 className='text-center text-4xl text-red-500'>Vite + React</h1>
+       <button className='btn btn-secondary text-center'>secondary</button>
+    </div>
+   
+        
+     
+    </>
+  )
+}
+
+export default App
