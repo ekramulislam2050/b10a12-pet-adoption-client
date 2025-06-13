e445f0986ece27550a44544a6b7097aeca72725c
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-06-13 10:50:10 +0600

    added context API

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+ 
+export  const AuthContext =createContext(null)
+const AuthProvider = ({children}) => {
+    const [user,setUser]=useState()
+    const [loading,setLoading]=useState()
+
+    const info={
+
+    }
+    return (
+        <AuthContext.Provider value={info}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
