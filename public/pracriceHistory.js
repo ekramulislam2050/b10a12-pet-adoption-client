26d38f52cff618fa13db69db27a92ed5fac4dde3
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-07-06 12:17:35 +0600

    complete how it work section

--- public/pracriceHistory.js
+++ public/pracriceHistory.js
@@ -2,32 +2,3 @@
 // *.bg-[#FFF1F4]
 // * formik and Yup
 //  */
-
-
-import leaf from "../../assets/logo/leaf213.jpg";
-
-const Heading = ({ headingMsg }) => {
-  return (
-    <div className="relative flex flex-col items-center justify-center px-4 mt-8">
-      {/* Text */}
-      <h2
-        className="text-[#A47149] font-semibold text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4"
-        style={{ fontFamily: "kapakana, sans-serif" }}
-      >
-        {headingMsg}
-      </h2>
-
-      {/* Image */}
-      <img
-        src={leaf}
-        alt="leaf"
-        className="w-full max-w-[600px] h-auto object-contain"
-      />
-    </div>
-  );
-};
-
-export default Heading;
-
-
-
--- src/Components/HowItWork/HowItWork.jsx
+++ src/Components/HowItWork/HowItWork.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import { FaSearch, FaPaw, FaUserCheck, FaHome } from "react-icons/fa";
 
 const HowItWork = () => {
+    const cardData = [
+        {
+            icon: <FaSearch className="text-4xl text-indigo-500" />,
+            title: "Browse Pets",
+            desc: "Find your perfect companion by browsing through available pets."
+        },
+        {
+            icon: <FaPaw className="text-4xl text-green-500" />,
+            title: "View Details",
+            desc: "Check pet details like age, breed, health status, and more."
+        },
+        {
+            icon: <FaUserCheck className="text-4xl text-yellow-500" />,
+            title: "Apply for Adoption",
+            desc: "Submit your application and wait for approval."
+        },
+        {
+            icon: <FaHome className="text-4xl text-pink-500" />,
+            title: "Bring Home",
+            desc: "Once approved, welcome your new furry friend home!"
+        }
+    ];
     return (
-        <div>
-            <h1>how it work------------------</h1>
+        <div className="min-h-screen w-full bg-gradient-to-b from-[#A47149] to-[#607f33] py-16 px-6 flex flex-col justify-center items-center mt-5">
+            <h2 className="mb-12 text-5xl font-bold text-center text-white font-[kapakana]">🐾 How It Works</h2>
+            <div className="grid max-w-6xl grid-cols-1 gap-8 mx- auto md:grid-cols-2 lg:grid-cols-4">
+                {
+                    cardData.map((data, index) => <div key={index} className="p-6 text-center transition duration-300 bg-[#ffffff] shadow-md rounded-xl hover:shadow-xl">
+                        {data.icon}
+                        <h1  className="mb-2 text-xl font-semibold">{data.title}</h1>
+                        <p className="text-sm text-gray-600">{data.desc}</p>
+                    </div>)
+                }
+            </div>
+
         </div>
     );
 };
 
 export default HowItWork;
+
+
