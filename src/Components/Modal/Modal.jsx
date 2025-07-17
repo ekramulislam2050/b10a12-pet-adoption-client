98eb103ea4b379cfdf3bc7b62190eb5acc565c2f
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-07-17 12:18:57 +0600

    post formik data to db

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -2,16 +2,22 @@ import useAuth from '@/Hooks/Auth/useAuth';
 import { Formik, Field, Form } from 'formik';
 
 import img2 from "../../assets/Pet-Care-img/care-2.png"
+import useAxiosPublic from '@/Hooks/AxiosPublic/useAxiosPublic';
+import successMsg from '@/ReUseAbleFunction/SuccessMsg/successMsg';
+import errorMsg from '@/ReUseAbleFunction/ErrorMsg/errorMsg';
+import { useRef } from 'react';
+
 
 
 const Modal = ({ data }) => {
+    const modalRef = useRef()
     const { user } = useAuth()
-   
+    const axiosPublic = useAxiosPublic()
     console.log(user)
     const { name, age, category, image, location, postedDate, _id } = data || {}
     // console.log(data)
     return (
-        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <dialog ref={modalRef} id="my_modal_5" className="modal modal-bottom sm:modal-middle">
 
             <div className="modal-box">
                 <div className="flex flex-col items-center mt-5">
@@ -19,8 +25,8 @@ const Modal = ({ data }) => {
                     <h1 className="sm:text-2xl font-semibold   text-[#e48d11] text-3xl">{name}</h1>
                     <img src={img2} alt="img2" className="w-[20%]" />
                 </div>
-               <Formik
-                 enableReinitialize
+                <Formik
+                    enableReinitialize
                     initialValues={{
                         petName: name,
                         petImg: image,
@@ -31,19 +37,32 @@ const Modal = ({ data }) => {
                         userAddress: ""
                     }}
                     onSubmit={async (values) => {
-                        await new Promise((r) => setTimeout(r, 500));
-                        alert(JSON.stringify(values, null, 2));
-                        console.log(values)
-                        // modal close----------
-                        document.getElementById("my_modal_5").close()
+
+                        try {
+                            console.log(values)
+                            const res = await axiosPublic.post("/adoptPets", values)
+                            if (res.data.insertedId) {
+                                successMsg("post successful")
+                                //  modal close---------
+                                modalRef.current.close()
+                                document.activeElement?.blur()
+                            }
+
+
+                        } catch (err) {
+                            if (err) {
+                                errorMsg(err.message)
+                            }
+                        }
+
                     }}
                 >
                     <Form>
                         <label htmlFor="userName">Name </label>
-                        <Field id="userName" name="userName"  className="w-full input input-bordered" defaultValue={user?.displayName} />
+                        <Field id="userName" name="userName" className="w-full input input-bordered" readOnly disabled />
 
                         <label htmlFor="userEmail">Email</label>
-                        <Field id="userEmail" name="userEmail"  className="w-full input input-bordered" defaultValue={user?.email} />
+                        <Field id="userEmail" name="userEmail" className="w-full input input-bordered" readOnly disabled />
 
                         <label htmlFor="userPhone">Phone : </label>
                         <Field id="userPhone" name="userPhone" required className="w-full input input-bordered" placeholder="input your phone number" />
@@ -54,9 +73,9 @@ const Modal = ({ data }) => {
                         <button type="submit" className=" btn btn-primary bg-[#e48d11] w-full text-xl tracking-wide my-3">Submit</button>
 
                     </Form>
-                </Formik> 
+                </Formik>
 
-  
+
 
 
             </div>
