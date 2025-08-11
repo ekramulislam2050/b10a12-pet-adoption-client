168a935d1e7c7e38c839a1cc72acbee4f14b7032
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-08-11 12:13:15 +0600

    working on table

--- src/Components/MyDonationCampaigns/MyDonationCampaigns.jsx
+++ src/Components/MyDonationCampaigns/MyDonationCampaigns.jsx
@@ -4,38 +4,113 @@ import errorMsg from "@/ReUseAbleFunction/ErrorMsg/errorMsg";
 import Spinner from "@/ReUseAbleFunction/Spinner/Spinner";
 import { useQuery } from "@tanstack/react-query";
 
- 
+
+
 
 const MyDonationCampaigns = () => {
-    const {user}=useAuth()
-     const email=user?.email?.toLowerCase()
-     
-    const axiosSecure=useAxiosSecure()
-      const {data:dpData=[],isError,isLoading,error}=useQuery({
-        queryKey:["dpData",email],
-        queryFn:async()=>{
-            const res = await axiosSecure.get('/donationPayment',{params:{email}})
-            return res.data
-        
-        },
-              enabled: !!email,
-              staleTime:0
-      })
-       if(!email){
-        return
-      }
-       console.log(dpData)
-      if(isLoading){
-        return <Spinner isLoading={isLoading}></Spinner>
-      }
-      if(isError){
-        return errorMsg(error.message)
-      }
-    return (
-        <div>
-            <h1 className="text-white">my donation campaigns-----------{dpData.length}</h1>
-        </div>
-    );
+  const { user } = useAuth()
+  const email = user?.email?.toLowerCase()
+
+  const axiosSecure = useAxiosSecure()
+  const { data: dpData = [], isError, isLoading, error } = useQuery({
+    queryKey: ["dpData", email],
+    queryFn: async () => {
+      const res = await axiosSecure.get('/donationPayment', { params: { email } })
+      return res.data
+
+    },
+    enabled: !!email,
+    staleTime: 0
+  })
+  if (!email) {
+    return
+  }
+  console.log(dpData)
+  if (isLoading) {
+    return <Spinner isLoading={isLoading}></Spinner>
+  }
+  if (isError) {
+    return errorMsg(error.message)
+  }
+  return (
+    <div className="p-5">
+      <div className="overflow-x-auto border border-[#07c19f] rounded-lg">
+        <table className="table ">
+          {/* head */}
+          <thead className="">
+            <tr className="text-[#fcb700] border border-[#07c19f]  ">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Pet Name</th>
+              <th>Max Donation</th>
+              <th>Progress</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {
+              dpData.map(data => <tr className="border border-[#07c19f] text-[#ffffff]" key={data._id}>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="w-12 h-12 mask mask-squircle">
+                        <img
+                          src={data.campaignInfo.petPicture}
+                          alt="Avatar Tailwind CSS Component" />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{data.campaignInfo.petName}</div>
+                      {/* <div className="text-sm opacity-50">United States</div> */}
+                    </div>
+                  </div>
+                </td>
+                <td>
+
+                  <p className="badge badge-ghost badge-sm">{data.campaignInfo.maximumDonationAmount
+                  }<span className="text-sm font-extrabold text-orange-400">৳</span></p>
+                </td>
+                <td>Purple</td>
+                <th className="flex items-center justify-between">
+                  <div className="bg-[#ffffff] px-3 rounded-full">
+                    <button className="text-black btn btn-ghost btn-xs">Edit</button>
+                  </div>
+                  <div className="bg-[#ffffff] px-3 rounded-full">
+                    <button className="text-black btn btn-ghost btn-xs">Pause</button>
+                  </div>
+                  <div className="bg-orange-500 rounded-full">
+                    <button className="btn btn-ghost btn-xs ">Donators</button>
+                  </div>
+                </th>
+              </tr>)
+            }
+
+
+
+          </tbody>
+          {/* foot */}
+          <tfoot className="">
+            <tr className="text-[#ffffff]  border border-[#07c19f] ">
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyDonationCampaigns;
