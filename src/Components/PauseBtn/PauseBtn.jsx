4fa655dbad4b219946c9a8c732d1780edcf4b528
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-08-19 18:38:24 +0600

    complete status updating

--- src/Components/PauseBtn/PauseBtn.jsx
+++ src/Components/PauseBtn/PauseBtn.jsx
@@ -1,39 +1,47 @@
 import useAxiosSecure from "@/Hooks/AxiosSecure/useAxiosSecure";
 import errorMsg from "@/ReUseAbleFunction/ErrorMsg/errorMsg";
-import Spinner from "@/ReUseAbleFunction/Spinner/Spinner";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import successMsg from "@/ReUseAbleFunction/SuccessMsg/successMsg";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 
 
 const PauseBtn = ({ data }) => {
     const axiosSecure = useAxiosSecure()
-    const [newStatus, setNewStatus] = useState()
-    const { data: pauseData = {}, isLoading, isError, error } = useQuery({
-        queryKey: ['pauseData', data._id],
-        queryFn: async () => {
-            const res = await axiosSecure.patch(`/cdcData/${data._id}/status`, { status: newStatus })
-            return res.data
-        }
-    })
-    console.log("pauseBtn=",pauseData)
-    if (isLoading) {
-        return <Spinner isLoading={isLoading}></Spinner>
-    }
-    if (isError) {
-        return errorMsg(error.message)
-    }
-    const handleClick = () => {
-        const status = data.status === "Active" ? "Pause" : "Active"
-        setNewStatus(status)
-    }
-    return (
+    const queryClient = useQueryClient()
 
-        <div className={`${data.status === "active" ? "bg-green-500" : "bg-red-500"}   px-3 rounded-full`} >
-            <button className="text-black btn btn-ghost btn-xs"
-                onClick={handleClick}
-            >Pause</button>
-        </div>
-    );
+    const { mutate, isPending } = useMutation({
+        mutationKey: ["statusToggling"],
+        mutationFn: async (newStatus) => {
+            const res = await axiosSecure.patch(`cdcData/${data._id}/status`, { status: newStatus })
+            return res.data
+        },
+        onSuccess: (res) => {
+            successMsg(`status update to ${res.status}`)
+            queryClient.invalidateQueries({
+                queryKey: ["dpData", data.email.toLowerCase()],
+            });
+        },
+         onError: (err) => {
+            errorMsg(err.message)
+        },
+    })
+       
+
+
+
+const handleClick = () => {
+    const newStatus = data.status === "Active" ? "Pause" : "Active"
+    mutate(newStatus)
+}
+return (
+
+    <div className={`${data.status === "Active" ? "bg-green-500" : "bg-red-500"}   px-3 rounded-full text-[#ffffff] `} >
+        <button className="btn btn-ghost btn-xs"
+            onClick={handleClick}
+            disabled={isPending}
+        >{data.status === "Active" ? "Pause" : "Active"}</button>
+    </div>
+);
 };
 
 export default PauseBtn;
