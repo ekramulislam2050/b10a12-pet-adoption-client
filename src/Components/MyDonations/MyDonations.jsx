414198f6b1257a06fc339771feaff81e6e06372a
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-09-16 15:04:24 +0600

    responsiveness of myDonation is complete

--- src/Components/MyDonations/MyDonations.jsx
+++ src/Components/MyDonations/MyDonations.jsx
@@ -12,7 +12,7 @@ const MyDonations = () => {
    const axiosSecure = useAxiosSecure()
    const { user } = useAuth()
    const email = user?.email?.toLowerCase()
-   const { data: donatorData = [],isLoading,isError,error,refetch } = useQuery({
+   const { data: donatorData = [], isLoading, isError, error, refetch } = useQuery({
       queryKey: ["donatorData", user?.email],
       queryFn: async () => {
          const res = await axiosSecure.get("/donarDataByEmail", { params: { email } })
@@ -22,119 +22,123 @@ const MyDonations = () => {
    // console.log(donatorData)
 
    // handle delete --------------
-   const handleDelete=async(id)=>{
+   const handleDelete = async (id) => {
       // console.log(id)
-         try{
-            const res = await axiosSecure.delete(`/refund/${id}`)
-            if(res.data.deletedCount===1){
-               successMsg("delete successful")
-               table.options?.meta?.refetch()
-            }
-         }catch(err){
-            errorMsg(err.message)
+      try {
+         const res = await axiosSecure.delete(`/refund/${id}`)
+         if (res.data.deletedCount === 1) {
+            successMsg("delete successful")
+            table.options?.meta?.refetch()
          }
+      } catch (err) {
+         errorMsg(err.message)
+      }
    }
 
    // column definition-----------
    const columnHelper = createColumnHelper()
    const columns = [
-        columnHelper.accessor("petImg",{
-         id:"petImg",
-         header:()=><span className="text-orange-400">Pet Image</span>,
-         cell:(info)=>{
-            return(
-               <img src={info.getValue()} alt="petImg" className="rounded h-14 w-14"/>
+      columnHelper.accessor("petImg", {
+         id: "petImg",
+         header: () => <span className="text-orange-400 mr-3 sm:mr-0">Pet Image</span>,
+         cell: (info) => {
+            return (
+               <img src={info.getValue()} alt="petImg" className="rounded h-14 w-14" />
             )
          }
-        }),
-        columnHelper.accessor("petName",{
-         id:"petName",
-         header:()=><span className="text-orange-400">Pet Name</span>,
-         cell:(info)=><span className="text-[#ffffff]">{info.getValue()}</span>
-        }),
-        columnHelper.accessor("donationAmount",{
-          id:"donationAmount",
-          header:()=><span className="text-orange-400">Amount</span>,
-          cell:(info)=><span className="text-[#ffffff]">{info.getValue()}</span>
-        }),
-        columnHelper.display({
-         id:"refund",
-         header:()=><span className="text-orange-400">Refund</span>,
-         cell:(info)=>{
-            return(
-               <button className="btn"
-                onClick={()=>handleDelete(info.row.original._id)}
+      }),
+      columnHelper.accessor("petName", {
+         id: "petName",
+         header: () => <span className="text-orange-400 mr-3  sm:mr-0">Pet Name</span>,
+         cell: (info) => <span className="text-[#ffffff]">{info.getValue()}</span>
+      }),
+      columnHelper.accessor("donationAmount", {
+         id: "donationAmount",
+         header: () => <span className="text-orange-400 mr-3  sm:mr-0">Amount</span>,
+         cell: (info) => <span className="text-[#ffffff] ">{info.getValue()}</span>
+      }),
+      columnHelper.display({
+         id: "refund",
+         header: () => <span className="text-orange-400 mr-3  sm:mr-0">Refund</span>,
+         cell: (info) => {
+            return (
+               <button className="btn "
+                  onClick={() => handleDelete(info.row.original._id)}
                >Refund</button>
             )
          }
-        })
+      })
    ]
    // tanstack table instance---------
-    const table=useReactTable({
-      data:donatorData,
-      columns:columns,
-      getCoreRowModel:getCoreRowModel(),
-      meta:{refetch}
-    })
+   const table = useReactTable({
+      data: donatorData,
+      columns: columns,
+      getCoreRowModel: getCoreRowModel(),
+      meta: { refetch }
+   })
 
 
    return (
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto w-full  ">
          {isLoading && <Spinner isLoading={isLoading}></Spinner>}
          {isError && errorMsg(error.message)}
-         
-         {/* heading------------- */}
-          <h2 className="flex justify-center text-3xl font-semibold text-[teal] my-4">💝 My donation</h2>
-           {/* Short message under heading */}
-           <p className="flex justify-center text-[#ffffff] tracking-wide">You have donated <span className="mx-2 text-xl font-semibold text-orange-300">{donatorData.length} </span> campaigns.Thank you for your kindness!</p>
-         <table className="table">
-            {/* head */}
-            <thead>
-               {
-                  table.getHeaderGroups().map((headerGroup)=>{
-                     return(
-                        <tr key={headerGroup.id}>
-                           {
-                              headerGroup.headers.map(header=>{
-                                 return(
-                                     <th key={header.id}>
+
+         <div className="flex-col items-center flex">
+            {/* heading------------ */}
+            <h2 className="flex justify-center text-3xl font-semibold text-[teal] my-4">💝 My donation</h2>
+            {/* Short message under heading */}
+            <p className="sm:flex justify-center text-[#ffffff] tracking-wide text-sm">You have donated <span className="sm:mx-2 sm:text-xl font-semibold text-orange-300 text-sm">{donatorData.length} </span> campaigns.Thank you for your kindness!</p>
+         </div>
+         <div className="flex justify-center">
+            <table className="sm:table  ">
+               {/* head */}
+               <thead className=" ">
+                  {
+                     table.getHeaderGroups().map((headerGroup) => {
+                        return (
+                           <tr key={headerGroup.id} className=" ">
+                              {
+                                 headerGroup.headers.map(header => {
+                                    return (
+                                       <th key={header.id} className="">
+                                          {
+                                             flexRender(header.column.columnDef.header, header.getContext())
+                                          }
+                                       </th>
+                                    )
+                                 })
+                              }
+                           </tr>
+                        )
+                     })
+                  }
+               </thead>
+               <tbody className="">
+                  {/* row 1 */}
+                  {
+                     table.getRowModel().rows.map((row) => {
+                        return (
+                           <tr key={row.id}>
+                              {row.getVisibleCells().map(cell => {
+                                 return (
+                                    <td key={cell.id}>
                                        {
-                                          flexRender(header.column.columnDef.header,header.getContext())
+                                          flexRender(cell.column.columnDef.cell, cell.getContext())
                                        }
-                                     </th>
+                                    </td>
                                  )
-                              })
-                           }
-                        </tr>
-                     )
-                  })
-               }
-            </thead>
-            <tbody>
-               {/* row 1 */}
-                {
-                  table.getRowModel().rows.map((row)=>{
-                     return(
-                        <tr key={row.id}>
-                           {row.getVisibleCells().map(cell=>{
-                              return(
-                                 <td key={cell.id}>
-                                    {
-                                       flexRender(cell.column.columnDef.cell,cell.getContext())
-                                    }
-                                 </td>
-                              )
-                           })}
-                        </tr>
-                     )
-                  })
-                }
+                              })}
+                           </tr>
+                        )
+                     })
+                  }
 
 
-            </tbody>
+               </tbody>
 
 
-         </table>
+            </table>
+         </div>
       </div>
    );
 };
