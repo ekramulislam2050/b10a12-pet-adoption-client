87d0400a020cf87662b1aaeba6b0b5a66123088b
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-10-01 17:56:14 +0600

    working on allPet

--- src/Components/AllPet/AllPet.jsx
+++ src/Components/AllPet/AllPet.jsx
@@ -1,6 +1,27 @@
  
+import useAxiosSecure from "@/Hooks/AxiosSecure/useAxiosSecure";
+import errorMsg from "@/ReUseAbleFunction/ErrorMsg/errorMsg";
+import Spinner from "@/ReUseAbleFunction/Spinner/Spinner";
+import { useQuery } from "@tanstack/react-query";
+
+ 
 
 const AllPet = () => {
+      const axiosSecure=useAxiosSecure()
+      const {data:allPetsData=[],isLoading,isError,error}=useQuery({
+        queryKey:["allPetsData"],
+        queryFn:async()=>{
+            const res = await axiosSecure.get("/allPet")
+            return res.data
+        }
+      })
+      console.log(allPetsData)
+      if(isLoading){
+        return <Spinner isLoading={true}></Spinner>
+      }
+      if(isError){
+        errorMsg(error.message)
+      }
     return (
         <div>
             <h1>all pet-------------</h1>
